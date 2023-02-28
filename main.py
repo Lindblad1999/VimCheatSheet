f0a617c0c18c3de8eb53972cbb19c4775d0e5ae1
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2023-02-28 13:53:30 +0100

    centering

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import shutil
 
 def cursor_movement():
     os.system('clear')
-    columns = shutil.get_terminal_size()
-    print(columns)
+    columns = shutil.get_terminal_size().columns
+    lines = shutil.get_terminal_size().lines
     keys = [('h', 'move cursor left'), ('j', 'move cursor down'),
         ('k', 'move cursor up'), ('l', 'move cursor right'), ('H', 'move to top of screen'),
         ('M', 'move to middle of screen'), ('L', 'move to bottom of screen'), ('w', 'jump fwd to start of word'),
@@ -15,9 +15,15 @@ def cursor_movement():
         ('G', 'go to last line of doc'), ('5gg', 'go to line 5'), ('Ctrl+e', 'move screen down one line(w.o. moving cursor)'),
         ('Ctrl+y', 'move screen up one line(w.o. moving cursor)') 
     ]
-    
+
+    # center in height
+    for lines in range(0, lines-(len(keys)*2)-5):
+        print('')
+
+    print(colored('CURSOR MOVEMENT'.center(columns), 'white'))
     for pair in keys:
-        print(colored(f'{pair[0]}        {pair[1]}', 'black', 'on_green'))
+        spaces = ' '*(75-len(pair[0])-len(pair[1]))
+        print(colored(f'{pair[0]}{spaces}{pair[1]}\n'.center(columns), 'white'))
 
 def run():
     while True:
